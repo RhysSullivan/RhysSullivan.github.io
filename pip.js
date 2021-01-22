a31ac8d9e33f44a5114f1366d541b420f84da340
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-01-21 20:00:15 -0800

    support for mobile share

--- pip.js
+++ pip.js
@@ -8,11 +8,23 @@ https://github.com/einaregilsson/Redirector
 
 function embedVideo() {
 		let html = ""; 
-		let filter = $("#search-input").val()	
+		let filter = $("#search-input").val()			
 		var n = filter.search("v=");		
-		html = `<iframe width="450" height="212" src="https://www.youtube.com/embed/` 
-		html += filter.substr(n+2);
-		html += `" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>`
+		if(n <= -1)
+		{
+			//https://youtu.be/X5CZxkYrrK8
+			n = filter.search("tu.be")
+			console.log(n)
+			html = `<iframe width="450" height="212" src="https://www.youtube.com/embed/` 
+			html += filter.substr(n+5);
+			html += `" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>`
+		}
+		else
+		{		
+			html = `<iframe width="450" height="212" src="https://www.youtube.com/embed/` 
+			html += filter.substr(n+2);
+			html += `" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>`
+		}
         $("div.row").append(html);
 		console.log("hi")
 }
